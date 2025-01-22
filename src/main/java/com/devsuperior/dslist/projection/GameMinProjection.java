2d6ae759d8dd4ed6d614717d2898171c7268d180
yadelph <yasmindelphim@gmail.com>
2025-01-22 20:40:04 -0300

    Controller, Service, Repository, Projection

---
+++ src/main/java/com/devsuperior/dslist/projection/GameMinProjection.java
@@ -0,0 +1,11 @@
+package com.devsuperior.dslist.projection;
+
+public interface GameMinProjection {
+
+    Long getId();
+    String getTitle();
+    Integer getGameYear();
+    String getImgUrl();
+    String getShortDescription();
+    Integer getPosition();
+}
